02b8a43c0d85dce3b585584aec0fc5e94d5dfc80
Yiwei <zyw870315@163.com>
2019-02-26 02:11:08 -0500

    Sync data between activities

--- app/src/main/java/com/comp1601/tipntax/MainActivity.java
+++ app/src/main/java/com/comp1601/tipntax/MainActivity.java
@@ -1,5 +1,7 @@
 package com.comp1601.tipntax;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,47 +9,67 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private Button calButton;
-    private Button visitButton;
-    private Button sendButton;
+    private Button mCalButton;
+    private Button mVisitButton;
+    private Button mSendButton;
+
+    private TextView mWeburlTxt;
+    private TextView mAmountTxt;
+    private TextView mEmailTxt;
+    private TextView mMessageTxt;
 
     private final static String TAG= "ButtonClick"  ;
+    public static final String TIP_N_TAX_MAIN_AMOUNT = "tip_n_tax_main_activity_amount";
+    private static final int TIP_N_TAX_TOTAL_RESULT = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button mCalButton = (Button) findViewById(R.id.calButton);
-        Button visitButton = (Button) findViewById(R.id.visitButton);
-        Button sendButton = (Button) findViewById(R.id.sendButton);
+        mCalButton = (Button) findViewById(R.id.calButton);
+        mVisitButton = (Button) findViewById(R.id.visitButton);
+        mSendButton = (Button) findViewById(R.id.sendButton);
 
 
-        TextView weburlTxt=(TextView) findViewById(R.id.weburl_text);
-        TextView amountTxt=(TextView) findViewById(R.id.amount_text);
-        TextView emailTxt=(TextView) findViewById(R.id.email_text);
-        TextView messageTxt=(TextView) findViewById(R.id.message_text);
+        mWeburlTxt=(TextView) findViewById(R.id.weburl_text);
+        mAmountTxt=(TextView) findViewById(R.id.amount_text);
+        mEmailTxt=(TextView) findViewById(R.id.email_text);
+        mMessageTxt=(TextView) findViewById(R.id.message_text);
 
 
         mCalButton.setOnClickListener(v->{
             Log.i(TAG,"CalButton Clicked");
-            TipNTaxCalculator cal= new TipNTaxCalculator(13,20);
-            double amount = Double.valueOf(amountTxt.getText().toString());
-            double ret = cal.calculate(amount);
-            Log.i(TAG,"Amount =" + amount);
-            String retStr = String.format("%.2f",ret);
-            Log.i(TAG,"Total"+retStr);
-            amountTxt.setText(retStr);
+            String amountStr = mAmountTxt.getText().toString();
+            double amount = Double.valueOf(amountStr);
+
+
+            Intent intent =new Intent(MainActivity.this,TipCalcActivity.class);
+            intent.putExtra(TIP_N_TAX_MAIN_AMOUNT,amount);
+            startActivityForResult(intent, TIP_N_TAX_TOTAL_RESULT);
 
         });
 
-        messageTxt.setOnClickListener(v-> {
-            messageTxt.setHint(null);
+        mMessageTxt.setOnClickListener(v-> {
+            mMessageTxt.setHint(null);
         });
 
-        amountTxt.setOnClickListener(v-> {
-            amountTxt.setHint(null);
+        mAmountTxt.setOnClickListener(v-> {
+            mAmountTxt.setHint(null);
         });
+
+
+    }
+    @Override
+    protected  void onActivityResult(int requestCode,int resultCode, Intent data){
+        if(resultCode!=Activity.RESULT_OK) return;
+        if (requestCode == TIP_N_TAX_TOTAL_RESULT){
+            if (data == null) return;
+            double computedTotal = data.getDoubleExtra(TipCalcActivity.TIP_N_TAX_COMPUTED_TOTAL,TipNTaxCalculator.InvalidResult);
+            String retStr = String.format("%.2f",computedTotal);
+            mAmountTxt.setText(retStr);
+        }
+
     }
 
 }
--- app/src/main/java/com/comp1601/tipntax/TipCalcActivity.java
+++ app/src/main/java/com/comp1601/tipntax/TipCalcActivity.java
@@ -1,14 +1,54 @@
 package com.comp1601.tipntax;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class TipCalcActivity extends AppCompatActivity {
+    private Button tCalButton;
+    private TextView tPriceTxt;
+    private TextView tPercentTxt;
+    private TextView tTotalTxt;
+
+    private final static String TAG= "ButtonClick"  ;
+    public static final String TIP_N_TAX_COMPUTED_TOTAL = "tip_n_tax_computed_total";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tip_calc);
-    }
 
+        tCalButton = (Button) findViewById(R.id.tCalButton) ;
+        tPriceTxt =(TextView) findViewById(R.id.price);
+        tPercentTxt = (TextView) findViewById(R.id.percent);
+        tTotalTxt = (TextView) findViewById(R.id.total);
+
+        double price =getIntent().getDoubleExtra(MainActivity.TIP_N_TAX_MAIN_AMOUNT,0.0);
+        String priceStrInit=""+price;
+        tPriceTxt.setText(priceStrInit);
+
+        tCalButton.setOnClickListener( v->{
+            Log.i(TAG,"TipCalcActivity:: CalButton Clicked");
+            String priceStr = tPriceTxt.getText().toString();
+            String tipStr = tPercentTxt.getText().toString();
+            //get the price and tip percentage string.
+            double priceAmount = Double.parseDouble(priceStr);
+            double tipAmount = Double.parseDouble(tipStr);
+            TipNTaxCalculator cal= new TipNTaxCalculator(13,tipAmount);
+
+            double ret = cal.calculate(priceAmount);
+            Log.i(TAG,"TipCalcActivity:: Amount =" + priceAmount);
+            String retStr = String.format("%.2f",ret);
+            Log.i(TAG,"TipCalcActivity:: Total"+retStr);
+            tTotalTxt.setText(retStr);
+
+            Intent data= new Intent();
+            data.putExtra(TIP_N_TAX_COMPUTED_TOTAL,ret);
+            setResult(RESULT_OK,data);
+
+        });
+    }
 }
